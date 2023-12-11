d067e28c35306769c609ab4d1eaf684e1f7816c9
giasong123 <fnvl8323@gmail.com>
2023-12-11 16:07:07 +0900

    docs : 1차 협업 프로젝트 mimi 프로젝트 셋팅

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/about/About";
+import Calendar from "./pages/calendar/Calendar";
+import Chart from "./pages/chart/Chart";
+import Feel from "./pages/feel/Feel";
+import Intro from "./pages/intro/Intro";
+import { Wrap } from "./styles/basic";
+
+const App = () => {
+  return (
+    <Wrap maxw={960}>
+      <Routes>
+        <Route path="/" element={<Intro />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/calendar" element={<Calendar />}></Route>
+        <Route path="/chart" element={<Chart />}></Route>
+        <Route path="/Feel" element={<Feel />}></Route>
+        <Route path="*" element={<h1>파일이없네요.</h1>}></Route>
+      </Routes>
+    </Wrap>
+  );
+};
+
+export default App;
