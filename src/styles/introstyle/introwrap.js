422d32f694503f8bbeb7bf1722509313fd411d9b
zuicy99 <k.zuicy@gmail.com>
2023-12-13 14:30:23 +0900

    feat: intro page css 완성, 카테고리 css 완성

---
+++ src/styles/introstyle/introwrap.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const IntroWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  background-image: linear-gradient(to right, #fff 50%, #00042d 50%);
+  .jejumyeongjo * {
+    font-family: "Jeju Myeongjo", serif;
+  }
+`;
