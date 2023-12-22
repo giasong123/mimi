5a13ce05a503f7b7c74e0ee9bc8ce417f97b9919
kimdohyun <dev.dsa9925@gmail.com>
2023-12-22 09:45:44 +0900

    feat : 차트 기능 추가

--- src/api/emo/apichart.js
+++ src/api/emo/apichart.js
@@ -2,11 +2,46 @@ import axios from "axios";
 
 export const getChart = async (iuser, setChartData) => {
   try {
-    const ChartData = {};
     const res = await axios.get(`/api/emo/chart/${iuser}`);
     console.log(res.data);
-    setChartData();
+    setChartData(res.data);
   } catch (error) {
     console.log(error);
+    alert("서버 상태가 불안정합니다. 잠시 후 시도해 주세요.");
+    setChartData({
+      emoChart: [
+        {
+          emotionGrade: 5,
+          dayOfTheWeek: 0,
+        },
+        {
+          emotionGrade: 3,
+          dayOfTheWeek: 1,
+        },
+        {
+          emotionGrade: 3,
+          dayOfTheWeek: 2,
+        },
+        {
+          emotionGrade: 3,
+          dayOfTheWeek: 3,
+        },
+        {
+          emotionGrade: 3,
+          dayOfTheWeek: 4,
+        },
+        {
+          emotionGrade: 3,
+          dayOfTheWeek: 5,
+        },
+        {
+          emotionGrade: 5,
+          dayOfTheWeek: 6,
+        },
+      ],
+      good: 1,
+      normal: 5,
+      bad: 1,
+    });
   }
 };
